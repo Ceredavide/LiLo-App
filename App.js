ffec65e5dba761f57083f81a7b82d99c04a67a93
Davide <ceredavide@live.it>
2019-10-21 10:19:55 +0200

    aggiunto firebase

--- App.js
+++ App.js
@@ -6,18 +6,10 @@ import { createAppContainer, createSwitchNavigator } from "react-navigation";
 import TabNav from "./navigation/TabNav";
 import LoginScreen from "./screens/LoginScreen";
 
-import { Provider } from "react-redux";
-import { createStore } from "redux";
-import rootReducer from "./reducers/rootReducer";
-
-const store = createStore(rootReducer);
-
 export default class App extends React.Component {
   render() {
     return (
-      <Provider store={store}>
         <Screen />
-      </Provider>
     );
   }
 }
--- reducers/rootReducer.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-
-import homeReducer from './homeReducer'
-
-export default combineReducers({
-  home: homeReducer,
-});
