cd8eb44967a906b646662f1c9045dd8e52514885
Davide <ceredavide@live.it>
2019-11-04 01:17:33 +0100

    provando ad utilizzare l'header

--- navigation/stacks/HomeStack.js
+++ navigation/stacks/HomeStack.js
@@ -1,17 +1,35 @@
 import { createStackNavigator } from "react-navigation";
 
 import HomeScreen from "../../screens/HomeScreen";
+import ComunicazioniScreen from "../../screens/ComunicazioniScreen";
+
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp
+} from "react-native-responsive-screen";
 
 const HomeStack = createStackNavigator(
   {
-    Home: HomeScreen
+    Home: HomeScreen,
+    Comunicazioni: ComunicazioniScreen
   },
   {
     defaultNavigationOptions: {
-      header: null
+      headerStyle: {
+        backgroundColor: "#009fff",
+        height: hp("10%"),
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        fontSize: hp("5%")
+      }
     }
   }
 );
+
+HomeScreen.navigationOptions = {
+  title: "Home"
+}
 
 HomeStack.navigationOptions = {
   tabBarLabel: "Home"
