a1d174eb3b868c5a3cc1d90de642b5da5c7847db
Davide <ceredavide@live.it>
2019-11-04 01:15:47 +0100

    reso il componente una classe

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -12,8 +12,9 @@ import { fetchComunicazioni } from "../store/actions/comunicazioni";
 
 import TabHeader from "../components/TabHeader";
 import Card from "../components/home/Card";
+import IconButton from "../components/IconButton";
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation }) => {
   const [loading, setLoading] = useState(false);
   const data = useSelector(state => state.comunicazioni.comunicazioni);
   const dispatch = useDispatch();
@@ -30,7 +31,13 @@ const HomeScreen = () => {
 
   return (
     <SafeAreaView style={styles.container}>
-      <TabHeader title="Home" />
+      {/* <View style={{ flexDirection: "row" }}>
+        <TabHeader title="Home" />
+        <IconButton
+          name="edit"
+          action={() => navigation.navigate("Comunicazioni")}
+        />
+      </View> */}
       <View style={styles.containerList}>
         {loading ? (
           <ActivityIndicator />
@@ -62,8 +69,8 @@ const styles = StyleSheet.create({
   containerList: {
     flex: 1,
     padding: 5,
-    borderTopLeftRadius: 20,
-    borderTopRightRadius: 20,
+    // borderTopLeftRadius: 20,
+    // borderTopRightRadius: 20,
     alignItems: "center",
     justifyContent: "center",
     backgroundColor: "#F1F5F9"
