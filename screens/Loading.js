fb9b6f72c6842583a74d2b62012e1ea19f0d7a19
Davide <ceredavide@live.it>
2019-11-09 10:59:51 +0100

    rinominato i files

--- screens/Loading.js
+++ screens/Loading.js
@@ -1,32 +1,30 @@
 import React from "react";
+import { Alert } from "react-native";
 import { AppLoading } from "expo";
 import { Asset } from "expo-asset";
 import * as Font from "expo-font";
 
 const LoadingScreen = ({ handleEnd }) => {
   loadResourcesAsync = async () => {
-    console.log("comincio il caricamento");
     return Promise.all([
       Asset.loadAsync([
         require("../assets/images/icon.png"),
         require("../assets/images/logo.png"),
         require("../assets/images/scuola.jpeg")
       ]),
-      console.log("asset caricati"),
       Font.loadAsync({
         "open-sans-light": require("../assets/fonts/OpenSans-Light.ttf")
-      }),
-      console.log("font caricato")
+      })
     ]);
   };
 
   handleLoadingError = error => {
-    console.warn(error);
+    console.log(error),
+      Alert.alert("Qualcosa è andato storto, faresti meglio a riavviare l'app");
   };
 
   handleFinishLoading = () => {
     handleEnd();
-    console.log("finito");
   };
 
   return (
