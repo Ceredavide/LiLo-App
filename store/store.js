003af2c623f1b94e726660ab2103a42ce3c64ec3
Davide <ceredavide@live.it>
2019-11-11 01:19:57 +0100

    aggiunto lo store delle assenze

--- store/store.js
+++ store/store.js
@@ -3,10 +3,12 @@ import ReduxThunk from "redux-thunk";
 
 import proposteReducer from "./reducers/proposte";
 import comunicazioniReducer from "./reducers/comunicazioni";
+import assenzeReducer from "./reducers/assenze";
 
 const rootReducer = combineReducers({
   proposte: proposteReducer,
-  comunicazioni: comunicazioniReducer
+  comunicazioni: comunicazioniReducer,
+  assenze: assenzeReducer
 });
 
 const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
