adf1fbc1ffc6cb1fd6edeb8e0466e2f867d18f27
Davide <ceredavide@live.it>
2019-11-11 01:21:44 +0100

    aggiunto possibilita di scegliere colore e style per il bottone

--- components/IconButton.js
+++ components/IconButton.js
@@ -4,10 +4,10 @@ import { TouchableOpacity } from "react-native";
 import { heightPercentageToDP as hp } from "react-native-responsive-screen";
 import { AntDesign } from "@expo/vector-icons";
 
-const IconButton = ({ name, action }) => {
+const IconButton = ({ name, action, color, style }) => {
   return (
-    <TouchableOpacity onPress={action}>
-      <AntDesign name={name} size={hp("4%")} color="white" />
+    <TouchableOpacity onPress={action} style={style}>
+      <AntDesign name={name} size={hp("4%")} color={color} />
     </TouchableOpacity>
   );
 };
