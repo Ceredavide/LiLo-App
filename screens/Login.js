16961a5e5c08131cbb75c10ac7e239a12d7f3185
Davide <ceredavide@live.it>
2019-11-18 17:48:32 +0100

    Creato la pagina di login definitiva

--- screens/Login.js
+++ screens/Login.js
@@ -1,46 +1,75 @@
 import React from "react";
-
-import { StyleSheet, KeyboardAvoidingView, View, Image } from "react-native";
+import { StyleSheet, View, StatusBar, Image, Text } from "react-native";
 
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp
 } from "react-native-responsive-screen";
 
-import LoginCard from "../components/login/LoginCard";
+import FormLogin from "../components/login/Form";
+import TouchableText from "../components/TouchableText";
 
 const LoginScreen = ({ navigation }) => {
   return (
-    <KeyboardAvoidingView behavior="padding" style={styles.containerLogin}>
-      {/* <Image
-        style={styles.imgLogo}
-        source={require("../assets/images/Logo.png")}
-      /> */}
+    <View style={styles.container}>
+      <StatusBar backgroundColor="white" barStyle="dark-content" />
+      <Text style={styles.text}>LiLo App</Text>
       <Image
-        style={styles.imgScuola}
+        style={styles.image}
         source={require("../assets/images/scuola.jpeg")}
       />
-      <LoginCard navigation={navigation} />
-    </KeyboardAvoidingView>
+      <FormLogin />
+      <View style={styles.buttons}>
+        <Text style={styles.textFooter}>Non hai ancora un account?</Text>
+        <TouchableText
+          action={() => navigation.navigate("SignUp")}
+          text="Registrati"
+          textStyle={{
+            fontSize: 20,
+            color: "#009fff",
+            fontFamily: "open-sans-regular",
+            textDecorationLine: "underline"
+          }}
+        />
+      </View>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  containerLogin: {
-    flex: 1,
-    backgroundColor: "#009fff",
-    alignItems: "center",
-    justifyContent: "center"
+  container: {
+    flex: 1
   },
-  imgLogo: {
-    height: 130,
-    width: 150,
-    marginBottom: 30
+  header: {
+    width: wp("100%"),
+    height: hp("35%"),
+    paddingTop: hp("5%"),
+    paddingBottom: hp("5%"),
+    borderBottomLeftRadius: 25,
+    borderBottomRightRadius: 25
   },
-  imgScuola: {
+  text: {
+    fontSize: hp("7%"),
+    alignSelf: "center",
+    color: "#009fff",
+    paddingTop: hp("8%"),
+    marginBottom: hp("3%"),
+    fontFamily: "open-sans-regular"
+  },
+  image: {
     height: hp("13%"),
     width: wp("85%"),
-    marginBottom: 10
+    marginTop: hp("3%"),
+    alignSelf: "center"
+  },
+  buttons: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+    paddingBottom: hp("5%")
+  },
+  textFooter: {
+    fontFamily: "open-sans-regular"
   }
 });
 
