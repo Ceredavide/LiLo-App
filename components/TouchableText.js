86c5efc8583cad5782a58fbc41b827d6f7eb54a5
Davide <ceredavide@live.it>
2019-11-18 17:49:19 +0100

    Creato componente per testo cliccabile

---
+++ components/TouchableText.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { TouchableOpacity, Text } from "react-native";
+
+const TouchableText = ({ text, action, textStyle, style }) => {
+  return (
+    <TouchableOpacity style={style} onPress={() => action()}>
+      <Text style={textStyle}>{text}</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default TouchableText;
