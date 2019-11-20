453e80a883058d17867f2063d37ed7c4d60306c7
Davide <ceredavide@live.it>
2019-11-20 23:42:45 +0100

    creato componente che si vede quando non ci sono assenze

---
+++ components/assenze/NoAssenze.js
@@ -0,0 +1,42 @@
+import React from "react";
+import {
+  StyleSheet,
+  ScrollView,
+  RefreshControl,
+  View,
+  Text
+} from "react-native";
+
+import { heightPercentageToDP as hp } from "react-native-responsive-screen";
+
+const NoAssenze = ({ isLoading, loadAssenze }) => {
+  return (
+    <ScrollView style={styles.container}>
+      <RefreshControl refreshing={isLoading} onRefresh={() => loadAssenze()} />
+      <View style={styles.card}>
+        <Text style={styles.emoji}>🤷🏻‍♂️</Text>
+        <Text style={styles.text}>Non sono previste assenze</Text>
+      </View>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+  },
+  card: {
+      marginTop: hp("20%"),
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  text: {
+    fontFamily: "open-sans-regular",
+    fontSize: hp("3%")
+  },
+  emoji: {
+    fontSize: hp("20%")
+  }
+});
+
+export default NoAssenze;
