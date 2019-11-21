895ede3b30a7edf9e2f6df54836d4e6dda9cc41e
Davide <ceredavide@live.it>
2019-11-21 21:01:01 +0100

    rimosso componenti react native paper

--- components/home/Card.js
+++ components/home/Card.js
@@ -1,13 +1,17 @@
 import React from "react";
-import { StyleSheet } from "react-native";
+import { StyleSheet, View, Image, Text } from "react-native";
 
-import { Card, Paragraph } from "react-native-paper";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp
+} from "react-native-responsive-screen";
 
 const HomeCard = ({ titolo, img, comunicazione }) => {
   return (
-    <Card>
-      <Card.Title title={titolo} titleStyle={styles.title} />
-      <Card.Cover
+    <View style={styles.card}>
+      <Text style={styles.title}>{titolo}</Text>
+      <Image
+        style={styles.image}
         source={{
           uri:
             img +
@@ -19,20 +23,42 @@ const HomeCard = ({ titolo, img, comunicazione }) => {
                 .toString(36)
                 .substring(2, 15)}`
         }}
-        style={styles.image}
       />
-      <Card.Content style={{ marginTop: 10 }}>
-        <Paragraph>{comunicazione}</Paragraph>
-      </Card.Content>
-    </Card>
+      <Text style={styles.paragraph}>{comunicazione}</Text>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
+  card: {
+    margin: wp("3%"),
+    backgroundColor: "white",
+    padding: wp("3%"),
+    borderRadius: 20,
+    backgroundColor: "#FFF",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1
+    },
+    shadowOpacity: 0.18,
+    shadowRadius: 1.0,
+
+    elevation: 1
+  },
   title: {
+    fontFamily: "open-sans-bold",
+    fontSize: hp("3%"),
     textAlign: "center"
   },
-  image: {}
+  paragraph: {
+    fontFamily: "open-sans-regular",
+    textAlign: "justify"
+  },
+  image: {
+    height: 30,
+    width: 40
+  }
 });
 
 export default HomeCard;
