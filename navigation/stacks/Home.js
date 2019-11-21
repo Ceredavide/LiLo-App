6cd73c9d72dceb23e4f4579b1ec41aae5dfa34a3
Davide <ceredavide@live.it>
2019-11-21 20:27:14 +0100

    Aggiunto pagina singola comunicazioni e settato header pagina comunicazioni

--- navigation/stacks/Home.js
+++ navigation/stacks/Home.js
@@ -1,27 +1,38 @@
 import { createStackNavigator } from "react-navigation";
 
+import { heightPercentageToDP as hp } from "react-native-responsive-screen";
+
 import HomeScreen from "../../screens/Home";
+import ComunicazioneScreen from "../../screens/Comunicazione";
 import ComunicazioniScreen from "../../screens/Comunicazioni";
 
-import HeaderStyle from "../../styles/navigation/Header"
+import HeaderStyle from "../../styles/navigation/Header";
 
 const HomeStack = createStackNavigator(
   {
     Home: HomeScreen,
+    Comunicazione: ComunicazioneScreen,
     Comunicazioni: ComunicazioniScreen
   },
   {
+    mode: "modal",
     defaultNavigationOptions: HeaderStyle
   }
 );
 
 HomeScreen.navigationOptions = {
   title: "Home"
-}
+};
 
 ComunicazioniScreen.navigationOptions = {
-  title: "Comunicazioni"
-}
+  title: "Comunicazioni",
+  headerTitleStyle: {
+    fontFamily: "playfair-regular",
+    fontWeight: "bold",
+    fontSize: hp("3%"),
+    marginBottom: hp("1%")
+  }
+};
 
 HomeStack.navigationOptions = {
   tabBarLabel: "Home"
