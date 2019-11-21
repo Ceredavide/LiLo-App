ae850d7dea24fa6381f1d8fbdfd2867fdef97598
Davide <ceredavide@live.it>
2019-11-21 21:12:49 +0100

    creato screen comunicazione

---
+++ screens/Comunicazione.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {StyleSheet, View, Text} from "react-native"
+
+const Comunicazione = () => {
+    return(
+        <View>
+            <Text>
+                Bellaaaaaaa
+            </Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    
+})
+
+export default Comunicazione
