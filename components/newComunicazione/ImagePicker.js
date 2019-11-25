dcb3043acd77fe7ee8fb2822efc5276d54ef43f7
Davide <ceredavide@live.it>
2019-11-25 14:16:23 +0100

    Creazione componente per selezionare immagine dal dispositivo

---
+++ components/newComunicazione/ImagePicker.js
@@ -0,0 +1,47 @@
+import React, { useEffect } from "react";
+import { Button, Image, View } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import Constants from "expo-constants";
+import * as Permissions from "expo-permissions";
+
+const ImagePickerExample = ({ image, setFieldValue }) => {
+  useEffect(() => {
+    getPermissionAsync();
+  }, []);
+
+  getPermissionAsync = async () => {
+    if (Constants.platform.ios) {
+      const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+      if (status !== "granted") {
+        alert("Sorry, we need camera roll permissions to make this work!");
+      }
+    }
+  };
+
+  _pickImage = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      aspect: [16, 9],
+      quality: 1
+    });
+
+    if (!result.cancelled) {
+      console.log(result)
+      setFieldValue("image", result.uri);
+    }
+  };
+
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Button title="Pick an image from camera roll" onPress={_pickImage} />
+      {
+        <Image
+          source={{ uri: image }}
+          style={{ width: 200, height: 200 }}
+        />
+      }
+    </View>
+  );
+};
+
+export default ImagePickerExample;
