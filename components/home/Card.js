cd0c747cfd94ca9f60cab4fb98d8c731ddf5d7cb
Davide <ceredavide@live.it>
2019-12-02 23:55:06 +0100

    rifatto card comunicazioni

--- components/home/Card.js
+++ components/home/Card.js
@@ -1,63 +1,58 @@
 import React from "react";
-import { StyleSheet, View, Image, Text } from "react-native";
+import { StyleSheet, TouchableOpacity, View, Image, Text } from "react-native";
 
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp
 } from "react-native-responsive-screen";
 
-const HomeCard = ({ titolo, img, comunicazione }) => {
+const HomeCard = ({img, titolo, sottotitolo, paragrafo }) => {
   return (
-    <View style={styles.card}>
-      <Text style={styles.title}>{titolo}</Text>
-      <Image
-        style={styles.image}
-        source={{
-          uri:
-            img +
-            "#" +
-            `${Math.random()
-              .toString(36)
-              .substring(2, 15) +
-              Math.random()
-                .toString(36)
-                .substring(2, 15)}`
-        }}
-      />
-      <Text style={styles.paragraph}>{comunicazione}</Text>
-    </View>
+    <TouchableOpacity style={styles.card}>
+      {/* <Image style={styles.image} source={{ uri: img }} /> */}
+      <View style={styles.containerText}>
+        <Text style={styles.title}>{titolo}</Text>
+        <Text style={styles.subtitle}>{sottotitolo}</Text>
+      </View>
+    </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
   card: {
-    margin: wp("3%"),
-    backgroundColor: "white",
-    padding: wp("3%"),
+    margin: wp("2%"),
+    width: wp("92%"),
+    flex: 1,
+    alignItems: "center",
+    flexDirection: "row",
     borderRadius: 20,
     backgroundColor: "#FFF",
     shadowColor: "#000",
     shadowOffset: {
       width: 0,
-      height: 1
+      height: 1,
     },
-    shadowOpacity: 0.18,
-    shadowRadius: 1.0,
-
-    elevation: 1
+    shadowOpacity: 0.20,
+    shadowRadius: 1.41,
+    elevation: 2,
+  },
+  image: {
+    height: hp("12%"),
+    width: hp("12%"),
+    margin: wp("2.5%"),
+    borderRadius: 20,
+  },
+  containerText:{
+    flex: 1 ,  //width (according to its parent)
+    justifyContent: 'center',
   },
   title: {
     fontFamily: "open-sans-bold",
-    fontSize: hp("3%"),
-    textAlign: "center"
+    fontSize: hp("2.5%"),
   },
-  paragraph: {
+  subtitle: {
     fontFamily: "open-sans-regular",
-    textAlign: "justify"
-  },
-  image: {
-    height: 30,
-    width: 40
+    padding: wp("2%"),
   }
 });
 
