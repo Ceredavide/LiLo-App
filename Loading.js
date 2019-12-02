ee5c62ef60360a715d4824319f11f2a3a9fe6ed7
Davide <ceredavide@live.it>
2019-12-02 18:55:23 +0100

    aggiornato fonts

--- Loading.js
+++ Loading.js
@@ -8,17 +8,20 @@ const LoadingScreen = ({ handleEnd }) => {
   loadResourcesAsync = async () => {
     return Promise.all([
       Asset.loadAsync([
-        require("../assets/images/icon.png"),
-        require("../assets/images/logo.png"),
-        require("../assets/images/scuola.jpeg"),
-        require("../assets/images/login_successfull.jpg"),
+        require("./assets/images/icon.png"),
+        require("./assets/images/logo.png"),
+        require("./assets/images/scuola.jpeg"),
+        require("./assets/images/login_successfull.jpg"),
+        require("./assets/images/student-hat.png")
       ]),
       Font.loadAsync({
-        "open-sans-light": require("../assets/fonts/OpenSans-Light.ttf"),
-        "open-sans-regular": require("../assets/fonts/OpenSans-Regular.ttf"),
-        "open-sans-bold": require("../assets/fonts/OpenSans-Bold.ttf"),
-        "playfair-regular": require("../assets/fonts/PlayfairDisplay-Regular.ttf"),
-        "robot-bold": require("../assets/fonts/Roboto-Bold.ttf")
+        "open-sans-light": require("./assets/fonts/OpenSans-Light.ttf"),
+        "open-sans-regular": require("./assets/fonts/OpenSans-Regular.ttf"),
+        "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+        "robot-bold": require("./assets/fonts/Roboto-Bold.ttf"),
+        "futura-medium" : require("./assets/fonts/Futura-medium.ttf"),
+        "futura-bold": require("./assets/fonts/Futura-Bold.ttf"),
+        "futura-book" : require("./assets/fonts/Futura-Book.ttf")
       })
     ]);
   };
