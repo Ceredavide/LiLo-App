de6a1ae59eda30ddfa035cdb826e50e50b8f4f9e
Davide <ceredavide@live.it>
2020-01-06 14:15:59 +0100

    creato servizio per fethcare le Assenze

---
+++ services/fetchAssenze.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export default fetchAssenze = async () => {
+    const response = await axios.get(
+        "http://liloautogestito.ch/API/assenze_docenti.py?ses=707bed165969b062c3265679688634664d67593c9fe8583bc445110cae30c790"
+    );
+    return response.data
+    // gestire errori
+}
