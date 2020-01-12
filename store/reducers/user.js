2d9700d0f38e47071e45bf96b0910b741521a188
Davide <ceredavide@live.it>
2020-01-12 22:58:13 +0100

    update

--- store/reducers/user.js
+++ store/reducers/user.js
@@ -1,40 +1,38 @@
+import * as actionTypes from "../actionTypes"
+
 const initialState = {
-    id: "",
-    nome: "",
-    cognome: "",
-    classe: "",
-    email: "",
-    isAdmin: ""
-  };
-  
-  const userReducer = (state = initialState, action) => {
-    switch (action.type) {
-      case "SAVE_USER_CREDENTIALS":
-        const { id, nome, cognome, classe, email } = action.payload;
-        const isAdmin =
-          email === "andrixmelone01@gmail.com" ||
-          email === "samuele.meschini@outlook.it" ||
-          email === "ceredavide@live.it";
-        return {
-          ...state,
-          id: id,
-          nome: nome,
-          cognome: cognome,
-          classe: classe,
-          isAdmin: isAdmin
-        };
-      case "DELETE_USER_CREDENTIALS":
-        return {
-          ...state,
-          id: "",
-          nome: "",
-          cognome: "",
-          classe: "",
-          isAdmin: ""
-        };
-      default:
-        return state;
-    }
-  };
-  
-  export default userReducer;
+  id: "",
+  nome: "",
+  cognome: "",
+  classe: "",
+  email: "",
+  isAdmin: ""
+};
+
+const userReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.SAVE_USER_CREDENTIALS:
+      const { _id, nome, cognome, classe, email } = action.user;
+      const isAdmin =
+        email === "andrixmelone01@gmail.com" ||
+        email === "samuele.meschini@outlook.it" ||
+        email === "ceredavide@live.it";
+      return {
+        ...state,
+        id: _id,
+        nome: nome,
+        cognome: cognome,
+        classe: classe,
+        isAdmin: isAdmin
+      };
+    case actionTypes.DELETE_USER_CREDENTIALS:
+      return {
+        ...state,
+        initialState
+      };
+    default:
+      return state;
+  }
+};
+
+export default userReducer;
