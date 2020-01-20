f764174ef0ff8bc7015bf8de65d38cccf2171920
Davide <ceredavide@live.it>
2020-01-20 02:11:33 +0100

    risolto problema con action
    ritornava undefined, ora ho gestito meglio la parte asyncrona

--- services/tryLogin.js
+++ services/tryLogin.js
@@ -2,19 +2,20 @@ import axios from "axios";
 import { AsyncStorage, Alert } from "react-native";
 
 export default tryLogin = async (email, password, navigation) => {
-    axios.post("https://cere.dev/authentication", {
-        email: email,
-        password: password,
-        strategy: "local"
-    }).then((response) => {
-        AsyncStorage.setItem("user", JSON.stringify(response.data))
-        navigation.navigate("App")
-        return response.data
-    })
-        .catch(error => {
-            if (error.response.status == 401) {
-                Alert.alert("username o password errati")
-            }
-            return null
+    try {
+        response = await axios.post("https://cere.dev/authentication", {
+            email: email,
+            password: password,
+            strategy: "local"
         })
+        user = response.data.user
+        await AsyncStorage.setItem("user", JSON.stringify(response.data))
+        navigation.navigate("App")
+    } catch (error) {
+        if (error.response.status == 401) {
+            Alert.alert("username o password errati")
+        }
+        user = null
+    }
+    return user;
 }
