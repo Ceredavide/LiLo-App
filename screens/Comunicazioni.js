bf920f2f3bca762c3bda39e2c4a6c09da7be5f2f
Davide <ceredavide@live.it>
2020-01-21 03:03:52 +0100

    rimosso view di troppo

--- screens/Comunicazioni.js
+++ screens/Comunicazioni.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
-import {useSelector} from "react-redux"
+import { useSelector } from "react-redux"
 
 import ListComunicazioni from "../components/comunicazioni/ListComunicazioni";
 import MyButton from "../components/MyButton";
@@ -9,14 +9,12 @@ const ComunicazioniScreen = ({ navigation }) => {
   const comunicazioni = useSelector(state => state.comunicazioni.comunicazioni)
   return (
     <View style={styles.container}>
-      <View style={styles.containerList}>
-        <MyButton
-          action={() => navigation.navigate("NewComunicazione")}
-          text="Nuova Comunicazione"
-          color="#1ed15a"
-        />
-        <ListComunicazioni comunicazioni={comunicazioni} />
-      </View>
+      <MyButton
+        action={() => navigation.navigate("NewComunicazione")}
+        text="Nuova Comunicazione"
+        color="#1ed15a"
+      />
+      <ListComunicazioni comunicazioni={comunicazioni} />
     </View>
   );
 };
@@ -24,13 +22,7 @@ const ComunicazioniScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#009fff"
-  },
-  containerList: {
-    flex: 1,
     padding: 5,
-    borderTopLeftRadius: 25,
-    borderTopRightRadius: 25,
     alignItems: "center",
     backgroundColor: "#F1F5F9"
   }
