b26bf6ff89b8b51410fa0bc71a7a329144d503ed
Davide <ceredavide@live.it>
2020-01-21 02:57:50 +0100

    creato componente touchableicon

---
+++ components/TouchableIcon.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { TouchableOpacity } from "react-native"
+
+import { AntDesign } from '@expo/vector-icons';
+
+const TouchableIcon = ({ name, color, action }) => {
+    return (
+        <TouchableOpacity style={{ flex: 1, marginRight: 15 }} onPress={() => action()}>
+            <AntDesign name={name} size={32} color={color} />
+        </TouchableOpacity>
+    )
+}
+
+
+export default TouchableIcon
