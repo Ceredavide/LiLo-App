95b5110aa0e491b273602fad4bedae71eb3d3f1f
Davide <ceredavide@live.it>
2020-01-21 03:03:08 +0100

    migliorato css

--- styles/navigation/Header.js
+++ styles/navigation/Header.js
@@ -5,14 +5,12 @@ import { heightPercentageToDP as hp } from "react-native-responsive-screen";
 export default styles = StyleSheet.create({
   headerStyle: {
     backgroundColor: "#009fff",
-    height: hp("8%"),
-    borderBottomWidth: 0
+    height: hp("7%"),
   },
   headerTitleStyle: {
     color: "white",
-    fontFamily: "futura-bold",
-    fontWeight: "bold",
-    fontSize: hp("5.5%"),
+    fontFamily: "System",
+    fontSize: hp("5%"),
     marginBottom: hp("1%")
   }
 });
