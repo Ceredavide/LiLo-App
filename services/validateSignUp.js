38f3048b3affd1a8e4a3e9dcdf052848c2e548b9
Davide <ceredavide@live.it>
2020-01-21 03:01:16 +0100

    creato funzione che convalidati i dati in signUp

--- services/validateSignUp.js
+++ services/validateSignUp.js
@@ -1,17 +1,33 @@
 export default validateSignUp = values => {
     const errors = {};
+    //
+    // nome
+    //
     if (!values.nome) {
         errors.nome = 'Il campo Nome è richiesto.';
     }
+    //
+    //cognome
+    //
     if (!values.cognome) {
         errors.cognome = "Il campo Cognome è richiesto.";
     }
+    //
+    // classe
+    //
     if (!values.classe) {
         errors.classe = "Il campo Classe è richiesto.";
     }
+    if (values.classe.length != 2 && values.classe.length != 3){
+        errors.classe ="La classe non è valida"
+    }
+    //
+    // email
+    //
     if (!values.email) {
         errors.email = "La Email è richiesta."
     }
+    // passwords
     if (!values.password) {
         errors.password = "Inserisci una password."
     }
