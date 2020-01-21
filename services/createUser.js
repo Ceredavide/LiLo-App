dc721856def322377846a864465492e5e72f3d30
Davide <ceredavide@live.it>
2020-01-21 03:01:29 +0100

    migliorato funzione con async

--- services/createUser.js
+++ services/createUser.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 import { Alert } from "react-native";
 
-export default createUser = (user, navigation) => {
+export default createUser = async (user, navigation) => {
     const userData = {
         nome: user.nome,
         cognome: user.cognome,
@@ -10,25 +10,24 @@ export default createUser = (user, navigation) => {
         password: user.password
 
     }
-
-    axios.post("https://cere.dev/users", userData)
-        .then(response => {
-            if (response.status == 201) {
-                Alert.alert("Account creato con successo!")
-                navigation.goBack()
-            }
-        })
-        .catch(error => {
-            console.log(error.response)
-            if (error.response.status == 409) {
-                Alert.alert("Un utente con queste credenziali esiste già")
-            }
-            // errore generato dal back-end quando non funza
-            else if (error.response.status == 502) {
-                Alert.alert("Qualcosa è andato storto")
-                // Qui non so quale sia l'errore, quindi lo printa e basta
-            } else {
-                Alert.alert(error.response.data.message)
-            }
-        })
+    try {
+        response = await axios.post("https://cere.dev/users", userData)
+        if (response.status == 201) {
+            Alert.alert("Account creato con successo!")
+            navigation.goBack()
+        }
+    } catch (error) {
+        console.log(error.response)
+        if (error.response.status == 409) {
+            Alert.alert("Un utente con queste credenziali esiste già")
+        }
+        // errore generato dal back-end quando non funza
+        else if (error.response.status == 502) {
+            Alert.alert("Qualcosa è andato storto")
+            // Qui non so quale sia l'errore, quindi lo printa e basta
+        } else {
+            Alert.alert(error.response.data.message)
+        }
+    }
+    return false
 };
