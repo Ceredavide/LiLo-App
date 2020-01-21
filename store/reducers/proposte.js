19d87b2a60b4f2a39fa0e570289416f14af4f921
Davide <ceredavide@live.it>
2020-01-21 03:02:42 +0100

    ora gestisce pure i dati del chart

--- store/reducers/proposte.js
+++ store/reducers/proposte.js
@@ -1,9 +1,13 @@
 import * as actionTypes from "../actionTypes"
 
+import handleError from "../../services/handleError"
+
 const initialState = {
   loadingList: false,
   loadingPost: false,
-  proposte: []
+  proposte: [],
+  classiArray: [],
+  numeriArray: []
 };
 
 const proposteReducer = (state = initialState, action) => {
@@ -12,13 +16,22 @@ const proposteReducer = (state = initialState, action) => {
       return { ...state, loadingList: true };
     }
     case actionTypes.FETCH_PROPOSTE_SUCCESS: {
+      const { data, classi } = action.proposte
+      const arrClassi = []
+      const arrNumeri = []
+      classi.sort((a, b) => { return b.n - a.n });
+      const newClassi = classi.splice(0, 5)
+      newClassi.forEach(classe => { arrClassi.push(classe.classe); arrNumeri.push(classe.n) })
       return {
         ...state,
-        proposte: action.proposte,
+        proposte: data,
+        classiArray: arrClassi,
+        numeriArray: arrNumeri,
         loadingList: false
       };
     }
     case actionTypes.FETCH_PROPOSTE_ERROR: {
+      handleError(action.error)
       return {
         ...state,
         loadingList: false
@@ -35,6 +48,7 @@ const proposteReducer = (state = initialState, action) => {
       };
     }
     case actionTypes.POST_PROPOSTA_ERROR: {
+      handleError(action.error)
       return { ...state, loadingPost: false };
     }
     default:
