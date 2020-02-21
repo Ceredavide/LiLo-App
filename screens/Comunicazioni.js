7d20be7527e1ece23a73c02701df132aa24bdfc2
Davide <ceredavide@live.it>
2020-02-21 13:59:51 +0100

    inserito flatList direttamente nello screen

--- screens/Comunicazioni.js
+++ screens/Comunicazioni.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, FlatList } from "react-native";
 import { useSelector } from "react-redux"
 
-import ListComunicazioni from "../components/comunicazioni/ListComunicazioni";
 import MyButton from "../components/MyButton";
+import CardComunicazione from "../components/comunicazioni/CardComunicazione"
 
 const ComunicazioniScreen = ({ navigation }) => {
   const comunicazioni = useSelector(state => state.comunicazioni.comunicazioni)
@@ -14,7 +14,18 @@ const ComunicazioniScreen = ({ navigation }) => {
         text="Nuova Comunicazione"
         color="#1ed15a"
       />
-      <ListComunicazioni comunicazioni={comunicazioni} />
+      <FlatList
+        data={comunicazioni}
+        keyExtractor={item => item._id}
+        showsVerticalScrollIndicator={false}
+        renderItem={({ item }) =>
+          <CardComunicazione
+            id={item._id}
+            titolo={item.titolo}
+            sottotitolo={item.sottotitolo}
+            immagine={item.immagine} />
+        }
+      />
     </View>
   );
 };
