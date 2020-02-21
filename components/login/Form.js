1c17fc131900f17f3779ea9c37df50d9b07f1094
Davide <ceredavide@live.it>
2020-02-21 14:19:51 +0100

    implementato hooks formik

--- components/login/Form.js
+++ components/login/Form.js
@@ -6,7 +6,7 @@ import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp
 } from "react-native-responsive-screen";
-import { Formik } from "formik";
+import { useFormik } from "formik";
 import { AntDesign } from "@expo/vector-icons";
 
 import tryLogin from "../../services/tryLogin"
@@ -21,65 +21,63 @@ const Form = ({ navigation }) => {
 
   handleLogin = async ({ email, password }) => {
     setLoading(true);
-    tryLogin(email, password, navigation).then(user => {
-      if (!!user) {
-        dispatch({ type: SAVE_USER_CREDENTIALS, user: user })
-      }
-      setLoading(false)
-    })
+    const user = await tryLogin(email, password, navigation)
+    if (!!user) {
+      dispatch({ type: SAVE_USER_CREDENTIALS, user: user })
+    }
+    setLoading(false)
+  }
 
-  };
+  const formikLogin = useFormik({
+    initialValues: { email: "", password: "" },
+    onSubmit: handleLogin
+  })
+
+  const { values, handleChange, handleSubmit } = formikLogin
 
   return (
-    <Formik
-      initialValues={{ email: "", password: "" }}
-      onSubmit={values => handleLogin(values)}
-    >
-      {props => (
-        <View style={styles.form}>
-          <View style={styles.containerTextInput}>
-            <AntDesign
-              name="user"
-              size={32}
-              color="#009fff"
-              style={styles.icon}
-            />
-            <TextInput
-              autoCapitalize="none"
-              placeholder="e-mail"
-              keyboardType="email-address"
-              style={styles.textInput}
-              onChangeText={props.handleChange("email")}
-              value={props.values.email}
-              returnKeyType="next"
-            />
-          </View>
-          <View style={{ ...styles.containerTextInput, marginBottom: hp("10%"), }}>
-            <AntDesign
-              name="lock"
-              size={32}
-              color="#009fff"
-              style={styles.icon}
-            />
-            <TextInput
-              placeholder="password"
-              secureTextEntry={true}
-              style={styles.textInput}
-              onChangeText={props.handleChange("password")}
-              value={props.values.password}
-              returnKeyType="next"
-            />
-          </View>
-          {/* Bottone password dimenticata */}
-          <LoadingButton
-            text="Login"
-            color="#009fff"
-            handleSubmit={props.handleSubmit}
-            loading={loading}
-          />
-        </View>
-      )}
-    </Formik>
+    <View style={styles.form}>
+      <View style={styles.containerTextInput}>
+        <AntDesign
+          name="user"
+          size={32}
+          color="#009fff"
+          style={styles.icon}
+        />
+        <TextInput
+          autoCapitalize="none"
+          placeholder="e-mail"
+          keyboardType="email-address"
+          style={styles.textInput}
+          onChangeText={handleChange("email")}
+          value={values.email}
+          returnKeyType="next"
+        />
+      </View>
+      <View style={{ ...styles.containerTextInput, marginBottom: hp("10%"), }}>
+        <AntDesign
+          name="lock"
+          size={32}
+          color="#009fff"
+          style={styles.icon}
+        />
+        <TextInput
+          placeholder="password"
+          secureTextEntry={true}
+          style={styles.textInput}
+          onChangeText={handleChange("password")}
+          value={values.password}
+          returnKeyType="next"
+        />
+      </View>
+      {/* Bottone password dimenticata */}
+      <LoadingButton
+        text="Login"
+        color="#009fff"
+        handleSubmit={handleSubmit}
+        loading={loading}
+      />
+    </View>
   );
 };
 
