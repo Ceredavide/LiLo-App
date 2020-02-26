2c70b921f67bd448fa560e222fa759a149ef3568
Davide <ceredavide@live.it>
2020-02-26 03:18:45 +0100

    aggiunto variabile style

--- components/TouchableIcon.js
+++ components/TouchableIcon.js
@@ -3,9 +3,9 @@ import { TouchableOpacity } from "react-native"
 
 import { AntDesign } from '@expo/vector-icons';
 
-const TouchableIcon = ({ name, color, action }) => {
+const TouchableIcon = ({ name, color, action, style={} }) => {
     return (
-        <TouchableOpacity style={{ flex: 1, marginRight: 15 }} onPress={action}>
+        <TouchableOpacity style={{ flex: 1, marginRight: 15, ...style }} onPress={action}>
             <AntDesign name={name} size={32} color={color} />
         </TouchableOpacity>
     )
