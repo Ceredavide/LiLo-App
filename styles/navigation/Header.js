8c2081b588291111f8cc3808761cae3a05e87801
Davide <ceredavide@live.it>
2020-02-26 03:17:54 +0100

    migliorato e globalizzato style header

--- styles/navigation/Header.js
+++ styles/navigation/Header.js
@@ -1,16 +1,19 @@
-import { StyleSheet } from "react-native";
-
 import { heightPercentageToDP as hp } from "react-native-responsive-screen";
 
-export default styles = StyleSheet.create({
+export default styles = {
   headerStyle: {
     backgroundColor: "#009fff",
-    height: hp("7%"),
+    height: hp("12%"),
   },
   headerTitleStyle: {
+    justifyContent: "center",
     color: "white",
     fontFamily: "System",
     fontSize: hp("5%"),
     marginBottom: hp("1%")
+  },
+  headerTintColor: "#fff",
+  headerBackTitleStyle: {
+    color: "#fff"
   }
-});
+}
