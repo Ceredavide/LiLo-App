59c53c2f433a9f0db1efbea1ae3dc4e4a8490dd6
Davide <ceredavide@live.it>
2020-02-26 03:19:09 +0100

    rinominato componente

--- App.js
+++ App.js
@@ -2,14 +2,14 @@ import React from "react"
 import { StatusBar } from "react-native"
 import { Provider } from "react-redux"
 
-import MainNavigator from "./navigation/MainNavigator"
 import store from "./store/store"
+import AppNavigator from "./navigation/AppNavigator"
 
 const App = () => {
     return (
         <Provider store={store}>
             <StatusBar backgroundColor="#009fff" barStyle="light-content" />
-            <MainNavigator />
+            <AppNavigator />
         </Provider>
     )
 }
