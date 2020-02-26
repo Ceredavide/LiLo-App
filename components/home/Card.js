3ce06dafe6e8ca09567412accdd4ce95db1de64a
Davide <ceredavide@live.it>
2020-02-27 00:28:01 +0100

    sku

--- components/home/Card.js
+++ components/home/Card.js
@@ -1,17 +1,18 @@
 import React from "react";
-import { StyleSheet, View, Image, Text } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp
 } from "react-native-responsive-screen";
 
-import LoadableImage from "./LoadableImage"
+import LoadableImage from "../home/LoadableImage"
 
-const HomeCard = ({ immagine, titolo, sottotitolo }) => {
+const HomeCard = ({ comunicazione }) => {
+  const { titolo, sottotitolo, immagine } = comunicazione
   return (
-    <View style={styles.container}>
-      <LoadableImage immagine={immagine} />
+    <View style={styles.card}>
+      <LoadableImage immagine={immagine}/>
       <View style={styles.containerText}>
         <Text style={styles.title}>{titolo}</Text>
         <Text style={styles.subtitle}>{sottotitolo}</Text>
@@ -38,23 +39,10 @@ const styles = StyleSheet.create({
     shadowRadius: 1.41,
     elevation: 2,
   },
-  container: {
-    flex: 1,
-    flexDirection: "row",
-    width: wp("92%"),
-    marginTop: hp("1.5%"),
-    alignItems: "center",
-    flexDirection: "row",
+  image: {
+    height: hp("12%"),
+    width: hp("12%"),
     borderRadius: 20,
-    backgroundColor: "#FFF",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 1,
-    },
-    shadowOpacity: 0.20,
-    shadowRadius: 1.41,
-    elevation: 2,
   },
   containerText: {
     marginTop: wp("4%"),
