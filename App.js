c71ad21193ab44eaff7cec4e0026354804097871
Davide <ceredavide@live.it>
2020-06-29 20:18:08 +0200

    implementato hook useCachedResources

--- App.js
+++ App.js
@@ -5,13 +5,23 @@ import { Provider } from "react-redux"
 import store from "./store/store"
 import AppNavigator from "./navigation/AppNavigator"
 
+import useCachedResources from "./hooks/useCachedResources"
+
 const App = () => {
-    return (
+    const { isLoadingComplete, user } = useCachedResources()
+
+    if (!isLoadingComplete) {
+        return null
+    }
+
+
+    else return (
         <Provider store={store}>
             <StatusBar backgroundColor="#009fff" barStyle="light-content" />
-            <AppNavigator />
+            <AppNavigator user={user} />
         </Provider>
     )
+
 }
 
 export default App;
