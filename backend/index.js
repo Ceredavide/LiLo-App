4a6c1a934b446e2a3b899b474b2913405a51d1a6
Davide <ceredavide@live.it>
2020-07-02 00:05:58 +0200

    implementato bodyParser e errorHandler

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,18 @@ const proposteRouter = require("./routes/proposte")
 
 const app = express();
 
+app.use(bodyParser.json())
+
 app.use('/api/comunicazioni', comunicazioniRouter)
 app.use('/api/proposte', proposteRouter)
+
+// aiuta a gestire gli errori
+app.use((error, req, res, next) => {
+    if (res.headerSent) {
+        return next(error)
+    }
+    res.status(error.code || 500)
+    res.json(error.message || "An unkown error ocurred.")
+})
 
 app.listen(5000)
