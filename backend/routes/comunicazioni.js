33daf5d84196e68f2b8cab4153b7749048b40add
Davide <ceredavide@live.it>
2020-07-02 09:38:44 +0200

    aggiunto route post createComunicazione

--- backend/routes/comunicazioni.js
+++ backend/routes/comunicazioni.js
@@ -1,11 +1,19 @@
 const express = require("express")
 
-const { getComunicazioni, getComunicazioniById } = require("../controllers/comunicazioni");
+const comunicazioniController = require("../controllers/comunicazioni");
+
+const {
+    getComunicazioni,
+    getComunicazioniById,
+    createComunicazione
+} = comunicazioniController
 
 const router = express.Router();
 
 router.get('/', getComunicazioni);
 
 router.get('/:id', getComunicazioniById)
+
+router.post('/', createComunicazione)
 
 module.exports = router;
