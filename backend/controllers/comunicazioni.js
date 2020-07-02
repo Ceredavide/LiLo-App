12eb3e052e30bb5b98dc1e97cd18f39715557adb
Davide <ceredavide@live.it>
2020-07-02 09:39:17 +0200

    aggiunto controller createComunicazione

--- backend/controllers/comunicazioni.js
+++ backend/controllers/comunicazioni.js
@@ -1,4 +1,5 @@
 const HttpError = require("../models/http-error")
+const { v4: uuid } = require("uuid")
 
 const DUMMY_DATA = [
     { id: 1, messaggio: "bella mate" },
@@ -12,7 +13,7 @@ const getComunicazioni = (req, res, next) => {
 }
 
 const getComunicazioniById = (req, res, next) => {
-    
+
     const id = req.params.id
     const comunicazione = DUMMY_DATA.find(c => c.id == id)
 
@@ -23,5 +24,14 @@ const getComunicazioniById = (req, res, next) => {
     res.json(comunicazione)
 }
 
+const createComunicazione = (req, res, next) => {
+    const { messaggio } = req.body
+
+    DUMMY_DATA.push({ id: uuid(), messaggio })
+
+    res.status(201).json({ message: "grande mate ce l'hai fatta" })
+}
+
 exports.getComunicazioni = getComunicazioni
 exports.getComunicazioniById = getComunicazioniById
+exports.createComunicazione = createComunicazione
