b62767b1ed852de77a403c9a87fee4f3b90ab7cf
Davide <ceredavide@live.it>
2020-07-02 09:40:36 +0200

    aggiunto global error e commenti vari

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,30 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 
+const HttpError = require("./models/http-error")
+
+// importazione gestore errori
+const errorHandler = require("./controllers/error")
+
+// importazioni routes api
 const comunicazioniRouter = require("./routes/comunicazioni")
 const proposteRouter = require("./routes/proposte")
 
 const app = express();
 
+// utilizzo parser che fa in modo che le risposte siano JSON
 app.use(bodyParser.json())
 
+// utilizzo API routes 
 app.use('/api/comunicazioni', comunicazioniRouter)
 app.use('/api/proposte', proposteRouter)
 
-// aiuta a gestire gli errori
-app.use((error, req, res, next) => {
-    if (res.headerSent) {
-        return next(error)
-    }
-    res.status(error.code || 500)
-    res.json(error.message || "An unkown error ocurred.")
+// route chiamata se si prova ad andare in un url senza routes
+app.use((req, res, next) => {
+    throw new HttpError("Could not find this route.", 404)
 })
+
+// utilizzo gestore errori
+app.use(errorHandler)
 
 app.listen(5000)
