1e0d1005143d45c4f260134021fca7e9b89da4f5
Davide <ceredavide@live.it>
2020-07-08 00:48:56 +0200

    implementato post comunicazione con db

--- backend/controllers/comunicazioni.js
+++ backend/controllers/comunicazioni.js
@@ -1,5 +1,7 @@
-const HttpError = require("../models/http-error")
 const { v4: uuid } = require("uuid")
+const HttpError = require("../models/http-error")
+
+const Comunicazione = require("../models/Comunicazione")
 
 const DUMMY_DATA = [
     { id: 1, messaggio: "bella mate" },
@@ -12,24 +14,45 @@ const getComunicazioni = (req, res, next) => {
     res.json(DUMMY_DATA)
 }
 
-const getComunicazioniById = (req, res, next) => {
+const getComunicazioniById = async (req, res, next) => {
 
     const id = req.params.id
-    const comunicazione = DUMMY_DATA.find(c => c.id == id)
+
+    let comunicazione;
+
+    try {
+        comunicazione = Comunicazione.findById(id)
+    } catch (err) {
+        return next(new HttpError("Errore nella ricerca della comunicazione, riprovare.", 500))
+    }
 
     if (!comunicazione) {
         return next(new HttpError("Ct non hai trovato nessuna comunicazione", 404))
     }
 
-    res.json(comunicazione)
+    res.json({ comunicazione: comunicazione.toObject({ getters: true }) })
 }
 
-const createComunicazione = (req, res, next) => {
-    const { messaggio } = req.body
+// const getComunicazioniByTagId = async
 
-    DUMMY_DATA.push({ id: uuid(), messaggio })
+const createComunicazione = async (req, res, next) => {
+    const { titolo, sottotitolo, paragrafo, immagine, creator } = req.body
 
-    res.status(201).json({ message: "grande mate ce l'hai fatta" })
+    const createdComunicazione = new Comunicazione({
+        titolo,
+        sottotitolo,
+        paragrafo,
+        immagine,
+        creator,
+    })
+    try {
+        await createdComunicazione.save();
+    } catch (err) {
+        const error = new HttpError("Errore nel salvataggio della comunicazione, riprovare.", 500)
+        return next(error)
+    }
+
+    res.status(201).json({ comunicazione: createdComunicazione })
 }
 
 exports.getComunicazioni = getComunicazioni
