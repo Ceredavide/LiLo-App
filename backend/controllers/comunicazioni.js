e1b2bb62a8b525786ed67c5b65dcab1e1814f32b
Davide <ceredavide@live.it>
2020-07-11 21:19:47 +0200

    aggiunto controllers per update e delete comunicazione

--- backend/controllers/comunicazioni.js
+++ backend/controllers/comunicazioni.js
@@ -48,13 +48,63 @@ const createComunicazione = async (req, res, next) => {
     try {
         await createdComunicazione.save();
     } catch (err) {
-        const error = new HttpError("Errore nel salvataggio della comunicazione, riprovare.", 500)
-        return next(error)
+        return next(new HttpError("Errore nel salvataggio della comunicazione, riprovare.", 500))
     }
 
     res.status(201).json({ comunicazione: createdComunicazione })
 }
 
+const updateComunicazione = async (req, res, next) => {
+    const id = req.params.id
+
+    const {
+        titolo,
+        sottotitolo,
+        paragrafo } = req.body
+
+    let comunicazione;
+
+    try {
+        comunicazione = await Comunicazione.findById(id)
+    } catch (err) {
+        return next(new HttpError("Errore nella modifica della comunicazione, riprovare.", 500))
+    }
+
+    comunicazione.titolo = titolo
+    comunicazione.sottotitolo = sottotitolo
+    comunicazione.paragrafo = paragrafo
+
+    try {
+        await comunicazione.save()
+    } catch (err) {
+        return next(new HttpError("Errore nel salvataggio della comunicazione modificata.", 500))
+    }
+
+    res.status(200).json({ comunicazione: comunicazione.toObject({ getters: true }) })
+}
+
+const deleteComunicazione = async (req, res, next) => {
+    const id = req.params.id
+
+    let comunicazione
+
+    try {
+        comunicazione = await Comunicazione.findById(id)
+    } catch (err) {
+        return next(new HttpError("Errore nella eliminazione della comunicazione, riprovare.", 500))
+    }
+
+    try {
+        await comunicazione.remove()
+    } catch (err) {
+        return next(new HttpError("Errore nella eliminazione della comunicazione, riprovare.", 500))
+    }
+
+    res.status(200).json({ id: comunicazione.toObject({ getters: true }).id })
+}
+
 exports.getComunicazioni = getComunicazioni
 exports.getComunicazioniById = getComunicazioniById
 exports.createComunicazione = createComunicazione
+exports.updateComunicazione = updateComunicazione
+exports.deleteComunicazione = deleteComunicazione
