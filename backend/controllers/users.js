b85db1bc15993cecfc2b1fb3fd74dac7faf1b5d5
Davide <ceredavide@live.it>
2020-07-12 20:35:04 +0200

    rimosso dummy users e signup e ologin funzionano con il backend

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,44 +1,71 @@
 const { v4: uuid } = require("uuid")
+const User = require("../models/User")
 
 const HttpError = require("../models/http-error")
 
-const DUMMY_USERS = []
+const getUsers = async (req, res, next) => {
 
-const getUsers = (req, res, next) => {
-    res.status(200).json({ users: DUMMY_USERS })
-}
+    let users
 
-const signup = (req, res, next) => {
-    const { name, email, password } = req.body
-
-    const user = DUMMY_USERS.find(u => u.email === email)
-
-    if (user) {
-        throw new HttpError("User with this email already exists.", 422)
+    try {
+        users = await User.find({}, '-password')
+    } catch (err) {
+        return next(new HttpError("Qualcosa è andato storto, riprova più tardi."))
     }
 
-    const newUser = {
-        id: uuid(),
-        name,
+    res.status(200).json({ users: users.map(user => user.toObject({ getters: true })) })
+}
+
+const signup = async (req, res, next) => {
+
+    const { nome, cognome, classe, email, password } = req.body
+
+    let existingUser
+
+    // controllo se esiste già un utente con quella password
+    try {
+        existingUser = await User.findOne({ email: email })
+    } catch (err) {
+        return next(new HttpError("Registrazione fallita, riprova più tardi.", 500))
+    }
+
+    if (existingUser) {
+        return next(new HttpError("É già presente un utente con la e-mail inserita.", 422))
+    }
+
+    const newUser = new User({
+        nome,
+        cognome,
+        classe,
         email,
         password
+    })
+
+    try {
+        await newUser.save()
+    } catch (err) {
+        return next(new HttpError("Registrazione fallita, riprova più tardi.", 500))
     }
 
-    DUMMY_USERS.push(newUser)
-
-    res.status(200).json({ user: newUser })
+    res.status(200).json({ user: newUser.toObject({ getters: true }) })
 }
 
-const login = (req, res, next) => {
+const login = async (req, res, next) => {
     const { email, password } = req.body
 
-    const selectedUser = DUMMY_USERS.find(u => u.email === email)
+    let existingUser
 
-    if (!selectedUser) {
-        throw new HttpError("User not found.", 401)
+    try {
+        existingUser = await User.findOne({ email: email })
+    } catch (err) {
+        return next(new HttpError("Login fallito, riprova più tardi.", 500))
     }
-    if (selectedUser.password !== password) {
-        throw new HttpError("Wrong password, try again.", 401)
+
+    if (!existingUser) {
+        return next(new HttpError("User not found.", 401))
+    }
+    if (existingUser.password !== password) {
+        return next(new HttpError("Wrong password, try again.", 401))
     }
 
     res.status(200).json({ message: "Succesfully logged in!" })
