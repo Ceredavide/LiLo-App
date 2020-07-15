0804822159c6ed54e22c02ac0752970f368a9259
Davide <ceredavide@live.it>
2020-07-15 20:00:08 +0200

    rimosso array comunicazioni e aggiunto nome proposte nel modello

--- backend/models/User.js
+++ backend/models/User.js
@@ -9,8 +9,10 @@ const userSchema = new Schema({
     classe: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true, minlength: 6 },
-    comunicazioni: [{ type: mongoose.Types.ObjectId, required: true, ref: "Comunicazione" }],
-    proposte: [{ type: mongoose.Types.ObjectId, required: true, ref: "Proposta" }]
+    proposte: [{
+        id: { type: mongoose.Types.ObjectId, required: true, ref: "Proposta" },
+        nome: { type: String, required: true }
+    }]
 }, {
     timestamps: true
 })
