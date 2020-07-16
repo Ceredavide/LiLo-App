e7e5527bfb099cd73ef0c39e5c7196196bf593a7
Davide <ceredavide@live.it>
2020-07-16 20:27:27 +0200

    aggiunto controllers a routes

--- backend/routes/proposte.js
+++ backend/routes/proposte.js
@@ -1,10 +1,15 @@
 const express = require("express")
 
+const proposteConstroller = require("../controllers/proposte")
+
+const { getProposte, getPropostaById, createProposta } = proposteConstroller
+
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    console.log("Bella Mate")
-    res.json({ message: "sembra funzioni" })
-});
+router.get('/', getProposte);
+
+router.get('/:id', getPropostaById);
+
+router.post('/', createProposta)
 
 module.exports = router;
