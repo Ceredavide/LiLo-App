b1a5c852b1720ecab1d9e2d9493cacd077d26996
Davide <ceredavide@live.it>
2020-08-04 18:17:43 +0200

    aggiunto pagina welcome a stack

--- frontend/navigation/stacks/Auth.js
+++ frontend/navigation/stacks/Auth.js
@@ -4,6 +4,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 
 import headerStyle from "../../styles/navigation/Header"
 
+import WelcomeScreen from "../../screens/Welcome"
 import LoginScreen from "../../screens/Login";
 import SignUpScreen from "../../screens/SignUp";
 
@@ -12,10 +13,15 @@ const { Navigator, Screen } = createStackNavigator()
 const AuthStack = () => {
   return (
     <Navigator>
-      <Screen name="Login" component={LoginScreen} options={loginOptions}/>
-      <Screen name="SignUp" component={SignUpScreen} options={{...headerStyle, ...signUpOptions}} />
+      <Screen name="Welcome" component={WelcomeScreen} options={welcomeOptions} />
+      <Screen name="Login" component={LoginScreen} options={loginOptions} />
+      <Screen name="SignUp" component={SignUpScreen} options={{ ...headerStyle, ...signUpOptions }} />
     </Navigator>
   )
+}
+
+const welcomeOptions = {
+  headerShown: false
 }
 
 const loginOptions = {
