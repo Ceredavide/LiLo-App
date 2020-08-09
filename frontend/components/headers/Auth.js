ae639377ae71b9bc9aa9768fb90866b8fcdcc401
Davide <ceredavide@live.it>
2020-08-10 00:44:19 +0200

    aumentato grandezza titolo

--- frontend/components/headers/Auth.js
+++ frontend/components/headers/Auth.js
@@ -39,7 +39,7 @@ const styles = StyleSheet.create({
         marginLeft: wp("5%")
     },
     title: {
-        fontSize: hp("4%"),
+        fontSize: hp("4.5%"),
         marginTop: -hp("2%"),
         color: Colors.secondary
     }
