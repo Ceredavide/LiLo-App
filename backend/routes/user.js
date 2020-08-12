26286f97eea2c4fc1771e2d482ed7a69db9b608b
Davide <ceredavide@live.it>
2020-08-13 01:16:05 +0200

    implementato check ruolo nella route get

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,11 +2,16 @@ const express = require("express")
 
 const usersController = require("../controllers/users")
 
+const checkAuth = require("../middlewares/checkAuth")
+const checkRole = require("../middlewares/checkRole")
+
+const ROLES = require("../constants/ROLES")
+
 const { signup, login } = usersController
 
 const router = express.Router()
 
-// router.get('/', getUsers)
+router.get('/', checkAuth, checkRole([ROLES.ADMIN]), getUsers)
 
 router.post('/signup', signup)
 
