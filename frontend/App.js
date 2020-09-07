540d2028ee30969dc46f08908f6eab2312109e77
Davide <ceredavide@live.it>
2020-09-07 14:06:59 +0200

    aggiunto StatusBar ad app

--- frontend/App.js
+++ frontend/App.js
@@ -1,10 +1,13 @@
 import React from "react"
+import { StatusBar } from "react-native"
 import { Provider } from "react-redux"
 
 import store from "./store/store"
 import AuthNavigator from "./navigation/AuthNavigator"
 
 import useCachedResources from "./hooks/useCachedResources"
+
+import Colors from "./constants/colors"
 
 const App = () => {
     const { isLoadingComplete, user } = useCachedResources()
@@ -15,6 +18,7 @@ const App = () => {
 
     else return (
         <Provider store={store}>
+            <StatusBar backgroundColor={Colors.main} barStyle="light-content" />
             <AuthNavigator user={user} />
         </Provider>
     )
