fc637e2d5df65b2eff97c255fbd876a03530ea50
Davide <ceredavide@live.it>
2020-09-07 14:06:42 +0200

    sostituito FlatList con map

--- frontend/components/autogestite/UltimeProposte.js
+++ frontend/components/autogestite/UltimeProposte.js
@@ -1,27 +1,23 @@
 import React from "react";
-import { FlatList } from "react-native";
 import { useSelector } from "react-redux"
 
 import CardProposta from "./CardProposta"
 import ProposteHolder from "./ProposteHolder"
 
 const UltimeProposte = () => {
+
   const isLoading = useSelector(state => state.proposte.loadingList)
   const proposte = useSelector(state => state.proposte.proposte)
 
-  if (isLoading) {
-    return <ProposteHolder />
-  } else {
-    return (
-      <FlatList
-        data={proposte}
-        keyExtractor={item => item._id}
-        renderItem={({ item }) =>
-          <CardProposta isLoading={isLoading} proposta={item} />
-        }
-      />
-    )
-  }
+  if (isLoading) return <ProposteHolder />
+
+  else return proposte.map((item, index) =>
+    <CardProposta
+      isLoading={isLoading}
+      proposta={item}
+      key={index}
+    />
+  )
 };
 
 export default UltimeProposte;
