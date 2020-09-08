bbca34413925da95e67e12e3ac246fe0c5018039
Davide <ceredavide@live.it>
2020-09-08 13:02:24 +0200

    migliorato stile

--- frontend/components/shared/IconWithText.js
+++ frontend/components/shared/IconWithText.js
@@ -1,10 +1,6 @@
 import React from "react"
 import { StyleSheet, View, Text } from "react-native"
 
-import {
-    heightPercentageToDP as hp
-} from "react-native-responsive-screen";
-
 import MaterialIcon from "react-native-vector-icons/MaterialCommunityIcons"
 
 const IconWithText = ({ iconName, text, iconSize, fontSize }) => {
@@ -20,8 +16,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         flexDirection: "row",
-        alignItems: "center",
-        marginTop: hp("0.5%")
+        alignItems: "center"
     },
     text: {
         fontSize: 5,
