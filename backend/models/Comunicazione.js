114fb7e792983491469ba3a5f6f727eea4105b34
Davide <ceredavide@live.it>
2020-09-17 11:12:04 +0200

    aggiunto tags ed editors

--- backend/models/Comunicazione.js
+++ backend/models/Comunicazione.js
@@ -6,7 +6,9 @@ const comunicazioneSchema = new Schema({
     titolo: { type: String, required: true },
     sottotitolo: { type: String, required: true },
     paragrafo: { type: String, required: true },
-    immagine: { type: String, required: true }
+    immagine: { type: String, required: true },
+    tags: [{ type: moongose.Types.ObjectId, ref: 'Tag' }],
+    editors: [{ type: moongose.Types.ObjectId, ref: 'User' }]
 }, {
     timestamps: true
 })
