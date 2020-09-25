d295dfaeae4d1cc2678d521cb4a0a8db569aaa84
Davide <ceredavide@live.it>
2020-09-25 23:14:24 +0200

    corretto funzione

--- frontend/utils/getContrastColor.js
+++ frontend/utils/getContrastColor.js
@@ -1,7 +1,21 @@
-export default function getContrastColor(hexcolor) {
-    let r = parseInt(hexcolor.substr(0, 2), 16);
-    let g = parseInt(hexcolor.substr(2, 2), 16);
-    let b = parseInt(hexcolor.substr(4, 2), 16);
-    let yiq = ((r * 299) + (g * 587) + (b * 114)) / 1000;
-    return (yiq >= 128) ? '#0000' : '#FFFF';
+export default function getContrastColor(hex) {
+
+    let threshold = 130; /* about half of 256. Lower threshold equals more dark text on dark background  */
+
+    let hRed = hexToR(hex);
+    let hGreen = hexToG(hex);
+    let hBlue = hexToB(hex);
+
+
+    function hexToR(h) { return parseInt((cutHex(h)).substring(0, 2), 16) }
+    function hexToG(h) { return parseInt((cutHex(h)).substring(2, 4), 16) }
+    function hexToB(h) { return parseInt((cutHex(h)).substring(4, 6), 16) }
+    function cutHex(h) { return (h.charAt(0) == "#") ? h.substring(1, 7) : h }
+
+    let cBrightness = ((hRed * 299) + (hGreen * 587) + (hBlue * 114)) / 1000;
+    if (cBrightness > threshold) {
+        return "#000000";
+    } else {
+        return "#ffffff";
+    }
 }
