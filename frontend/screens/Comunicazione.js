8dd60078fdba46583aaddc5220d504ffddcc69cc
Davide <ceredavide@live.it>
2020-09-25 11:05:42 +0200

    aggiunto visualizzazione tags

--- frontend/screens/Comunicazione.js
+++ frontend/screens/Comunicazione.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { StyleSheet, SafeAreaView, ScrollView, View, ImageBackground, Text } from "react-native"
 
 import {
@@ -6,46 +6,43 @@ import {
     heightPercentageToDP as hp
 } from "react-native-responsive-screen";
 
-import axios from "axios"
-
 import TransitionView from "../components/shared/TransitionView"
 import FloatingButton from "../components/shared/FloatingButton"
+import Tag from "../components/comunicazione/Tag"
 
 import Colors from "../constants/colors"
 
 const Comunicazione = ({ route, navigation }) => {
     const { comunicazione } = route.params
-    const { titolo, sottotitolo, paragrafo, immagine } = comunicazione
-    const [image, setImage] = useState(null)
+    const { titolo, sottotitolo, paragrafo, immagine, tags } = comunicazione
 
-
-    useEffect(() => {
-        axios.get(`https://cere.dev/uploads/${immagine}`)
-            .then(response => {
-                setImage(response.data.uri)
-            }).catch(error => setImage(null))
-    }, [])
+    function renderTags(tags) {
+        return tags.map((tag, index) => (
+            <Tag tag={tag} key={index} />
+        ))
+    }
 
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: Colors.main }}>
             <ScrollView style={styles.container}>
                 <View style={styles.containerImage}>
-                    {!image ? null :
-                        <TransitionView>
-                            <ImageBackground style={styles.image} source={{ uri: image }} >
-                                <FloatingButton
-                                    iconName="arrowleft"
-                                    action={navigation.goBack}
-                                    style={styles.floatingButton}
-                                    color={Colors.white}
-                                />
-                            </ImageBackground>
-                        </TransitionView>
-                    }
+                    <TransitionView>
+                        <ImageBackground style={styles.image} source={{ uri: immagine }} >
+                            <FloatingButton
+                                iconName="arrowleft"
+                                action={navigation.goBack}
+                                style={styles.floatingButton}
+                                color={Colors.white}
+                            />
+                        </ImageBackground>
+                    </TransitionView>
                 </View>
                 <View style={styles.containerText}>
                     <Text style={styles.title}>{titolo}</Text>
                     <Text style={styles.subtitle}>{sottotitolo}</Text>
+                    <View style={styles.containerTags}>
+                        {renderTags(tags)}
+                    </View>
                     <Text style={styles.paragraph}>{paragrafo}</Text>
                 </View>
             </ScrollView>
@@ -62,6 +59,13 @@ const styles = StyleSheet.create({
         alignSelf: "center",
         width: wp("100%"),
         height: wp("100%"),
+    },
+    containerTags: {
+        height: hp('4%'),
+        flexDirection: "row",
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginVertical: hp("2%"),
     },
     image: {
         width: wp("100%"),
@@ -84,7 +88,6 @@ const styles = StyleSheet.create({
     subtitle: {
         fontFamily: "open-sans-regular",
         fontSize: hp("2.5%"),
-        marginBottom: hp("3%"),
         color: Colors.white
     },
     containerText: {
