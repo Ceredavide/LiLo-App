c44160986070acda912dcf910df5b4224991097c
Davide <ceredavide@live.it>
2020-09-25 23:14:48 +0200

    importato tryLogin in hook, aggiunto Alert errore da backend

--- frontend/hooks/useLogin.js
+++ frontend/hooks/useLogin.js
@@ -1,20 +1,23 @@
 import { useState } from "react"
+import { Alert } from 'react-native'
 import { useDispatch } from "react-redux"
-
+import * as SecureStore from 'expo-secure-store';
 import { useFormik } from "formik";
 
 import loginSchema from "../validation/loginSchema"
 
 import { SIGN_IN } from "../store/actionTypes"
 
-import tryLogin from "../utils/tryLogin"
+import axios from "axios";
+
+import checkConnection from "../utils/checkConnection"
 
 const useLogin = () => {
     const dispatch = useDispatch()
 
     const [isLoading, setIsLoading] = useState(false);
 
-    handleLogin = async ({ email, password }) => {
+    async function handleLogin({ email, password }) {
         setIsLoading(true);
         try {
             const user = await tryLogin(email, password)
@@ -22,10 +25,27 @@ const useLogin = () => {
                 dispatch({ type: SIGN_IN, user: user })
             }
         } catch (error) {
-            console.log(error)
+            Alert.alert(error.response.data)
         } finally {
             setIsLoading(false)
         }
+    }
+
+    async function tryLogin(email, password) {
+
+        let user
+
+        await checkConnection()
+        const response = await axios.post("http://10.3.141.190:5000/api/users/login", {
+            email: email,
+            password: password
+        })
+
+        user = response.data.user
+        await SecureStore.setItemAsync("user", JSON.stringify(response.data))
+
+        return user
+
     }
 
     const formikLogin = useFormik({
