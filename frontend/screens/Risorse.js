fea14d9913d04a684d7a98352d8d99a0407fc753
Davide <ceredavide@live.it>
2020-09-25 11:05:59 +0200

    Creato Screen risorse

---
+++ frontend/screens/Risorse.js
@@ -0,0 +1,21 @@
+import React from "react"
+import { StyleSheet, View, Text } from "react-native"
+import Screen from "../components/shared/Screen"
+
+const Risorse = () => {
+    return (
+        <Screen>
+            <Text>Bella Mate</Text>
+        </Screen>
+    )
+}
+
+// const Card = () => {
+
+// }
+
+const styles = StyleSheet.create({
+
+})
+
+export default Risorse
