f0c81dd15222c4edcbf3c53c0fa2fb3e56d8cb94
Davide <ceredavide@live.it>
2020-09-25 23:14:03 +0200

    creato componente ErrorScreen

--- frontend/screens/Error.js
+++ frontend/screens/Error.js
@@ -1,20 +1,50 @@
 import React from "react"
 import {
     StyleSheet,
-    ScrollView,
-    RefreshControl,
     View,
     Text
 } from "react-native";
 
 import LottieView from 'lottie-react-native';
-import { heightPercentageToDP as hp } from "react-native-responsive-screen";
 
-const Error = () => {
+import {
+    heightPercentageToDP as hp,
+    widthPercentageToDP as wp
+} from "react-native-responsive-screen";
+
+import Button from "../components/shared/MyButton"
+
+import Colors from '../constants/colors'
+
+const Error = ({ reload, text }) => {
     return (
         <View style={{ flex: 1 }}>
+            <LottieView
+                autoPlay
+                loop={false}
+                style={styles.lottie}
+                source={require('../assets/lottie/error.json')}
+            />
+            <Text style={styles.text}>{text}</Text>
+            <Button text="Riprova" action={reload} color="red" />
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    lottie: {
+        width: hp('50%'),
+        height: hp('50%')
+    },
+    text: {
+        color: Colors.white,
+        fontFamily: 'open-sans-regular',
+        fontSize: hp('3.5%'),
+        alignSelf: "center",
+        textAlign: 'center',
+        width: wp('80%'),
+        marginBottom: hp('2%')
+    }
+})
 
 export default Error
