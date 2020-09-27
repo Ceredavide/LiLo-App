e145cb59c65fdecd8af6d405f781ddc826cfd6bd
Davide <ceredavide@live.it>
2020-09-27 20:31:44 +0200

    passato funzione fetchAssenze ad hook

--- frontend/hooks/useAssenze.js
+++ frontend/hooks/useAssenze.js
@@ -1,8 +1,13 @@
 import { useState, useEffect } from "react"
 
-import fetchAssenze from "../utils/fetchAssenze"
+import axios from "axios"
+
+import checkConnection from "../utils/checkConnection"
 
 const useAssenze = () => {
+
+    const URL_ASSENZE = "http://liloautogestito.ch/API/assenze_docenti.py?ses=707bed165969b062c3265679688634664d67593c9fe8583bc445110cae30c790"
+
     const [assenze, setAssenze] = useState([])
     const [isLoading, setIsloading] = useState(false)
     const [isRefreshing, setIsRefreshing] = useState(false)
@@ -10,6 +15,20 @@ const useAssenze = () => {
     useEffect(() => {
         handleFetch()
     }, [])
+
+    async function fetchAssenze() {
+        let assenze = []
+        try {
+            await checkConnection()
+            const response = await axios.get(URL_ASSENZE)
+            assenze = response.data
+        } catch (error) {
+            assenze = "error"
+            // handleError(error)
+        } finally {
+            return assenze
+        }
+    }
 
     const handleFetch = async () => {
         setIsloading(true)
