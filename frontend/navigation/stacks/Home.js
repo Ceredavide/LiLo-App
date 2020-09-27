e872b517f208df411fef4c86263943760a7270df
Davide <ceredavide@live.it>
2020-09-27 20:33:03 +0200

    passato comunicazioni context

--- frontend/navigation/stacks/Home.js
+++ frontend/navigation/stacks/Home.js
@@ -3,27 +3,32 @@ import React from "react"
 import { createStackNavigator } from "@react-navigation/stack";
 
 import {
-  widthPercentageToDP as wp,
   heightPercentageToDP as hp
 } from "react-native-responsive-screen";
 
 import HomeScreen from "../../screens/Home";
 import ComunicazioneScreen from "../../screens/Comunicazione";
+import ComunicazioniByTagScreen from '../../screens/ComunicazioniByTag'
 import ComunicazioniScreen from "../../screens/Comunicazioni";
 import NewComunicazioneScreen from "../../screens/NewComunicazione";
 
 import headerStyle from "../../styles/navigation/Header";
 
+import { ComunicazioniContext } from "../../Context"
+
 const { Navigator, Screen } = createStackNavigator()
 
 const HomeStack = () => {
   return (
-    <Navigator screenOptions={headerStyle}>
-      <Screen name="Home" component={HomeScreen} />
-      <Screen name="Comunicazione" component={ComunicazioneScreen} options={comunicazioneOptions} />
-      <Screen name="Comunicazioni" component={ComunicazioniScreen} options={comunicazioniOptions} />
-      <Screen name="Nuova Comunicazione" component={NewComunicazioneScreen} options={newComunicazioneOptions} />
-    </Navigator>
+    <ComunicazioniContext.Provider value="bella">
+      <Navigator screenOptions={headerStyle}>
+        <Screen name="Home" component={HomeScreen} />
+        <Screen name="Comunicazione" component={ComunicazioneScreen} options={comunicazioneOptions} />
+        <Screen name="ComunicazioniByTag" component={ComunicazioniByTagScreen} options={comunicazioneOptions} />
+        <Screen name="Comunicazioni" component={ComunicazioniScreen} options={comunicazioniOptions} />
+        <Screen name="Nuova Comunicazione" component={NewComunicazioneScreen} options={newComunicazioneOptions} />
+      </Navigator>
+    </ComunicazioniContext.Provider>
   )
 }
 
