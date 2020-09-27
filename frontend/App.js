fb7a5db5a95f2c3c6217a16432d9455a1b769cb3
Davide <ceredavide@live.it>
2020-09-27 20:30:25 +0200

    passato authNavigator ad App, aggiunto AuthContext

--- frontend/App.js
+++ frontend/App.js
@@ -1,29 +1,56 @@
-import React, { useState } from "react"
-import { StatusBar } from "react-native"
+import React, { useState, useMemo } from "react"
 import { Provider } from "react-redux"
+
+import { NavigationContainer } from "@react-navigation/native"
+import { createStackNavigator } from "@react-navigation/stack"
+
+import AuthStack from "./navigation/stacks/Auth";
+import AppNavigator from "./navigation/AppNavigator";
 
 import store from "./store/store"
 
-import AuthNavigator from "./navigation/AuthNavigator"
-import LoadingScreen from "./screens/Loading"
+import { AuthContext } from "./Context"
 
-import Colors from "./constants/colors"
+import Loading from "./screens/Loading";
+
+const { Navigator, Screen } = createStackNavigator()
 
 const App = () => {
+
     const [isLoading, setIsLoading] = useState(true)
+    const [auth, setAuth] = useState({})
 
-    return (
-        <Provider store={store}>
+    const providerAuth = useMemo(() => ({ auth, setAuth }), [auth, setAuth])
 
-            {isLoading ? <LoadingScreen setIsLoading={setIsLoading} /> :
-                <>
-                    <StatusBar backgroundColor={Colors.main} barStyle="light-content" />
-                    <AuthNavigator />
-                </>
-            }
+    if (isLoading) {
+        return <Loading setIsLoading={setIsLoading} setAuth={setAuth} />
+    }
 
-        </Provider>
-    )
+    else {
+        return (
+            <Provider store={store}>
+                <AuthContext.Provider value={providerAuth}>
+                    <NavigationContainer>
+                        <Navigator screenOptions={_screenOptions} mode="modal">
+                            {!auth.token ?
+                                <>
+                                    <Screen name="Auth" component={AuthStack} />
+                                </> :
+                                <>
+                                    <Screen name="App" component={AppNavigator} />
+                                </>
+                            }
+                        </Navigator>
+                    </NavigationContainer>
+                </AuthContext.Provider>
+            </Provider>
+        )
+    }
 }
+
+const _screenOptions = {
+    headerShown: false
+}
+
 
 export default App;
