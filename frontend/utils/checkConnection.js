a16bcb2f655cd1b9e9a162b6222f94102d8c8ca3
Davide <ceredavide@live.it>
2020-10-05 16:00:07 +0200

    corretto throw error

--- frontend/utils/checkConnection.js
+++ frontend/utils/checkConnection.js
@@ -1,12 +1,16 @@
 import * as Network from 'expo-network';
 
 export default async function checkConnection() {
-    
+
     const networkState = await Network.getNetworkStateAsync();
 
     const { isConnected, isInternetReachable } = networkState
 
     if (!isConnected || !isInternetReachable) {
-        throw "Connessione al server fallita, verifica la tua connessione ad Internet."
+        throw {
+            response: {
+                data: "Connessione fallita, verifica la tua connessione ad Internet."
+            }
+        }
     }
 }
