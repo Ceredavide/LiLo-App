16a25eed2ac7ff52ba3a55609da0ca2e909cded3
Davide <ceredavide@live.it>
2020-11-27 14:31:01 +0100

    rimosso classe e usato envVars

--- hooks/useSignUp.js
+++ hooks/useSignUp.js
@@ -1,48 +1,37 @@
 import { useState } from "react"
-import axios from "axios"
 import { Alert } from "react-native";
 import { useFormik } from "formik";
 
+import axios from "axios"
+
 import signUpSchema from "../validation/signUpSchema"
+
+import checkConnection from "../utils/checkConnection"
+
+import useEnvVars from "../configuration"
+
+const { apiUrl } = useEnvVars()
 
 const useSignUp = (navigation) => {
     const [isLoading, setIsLoading] = useState(false)
 
     async function createUser(user, navigation) {
-        const userData = {
-            nome: user.nome,
-            cognome: user.cognome,
-            classe: user.classe,
-            email: user.email,
-            password: user.password
-
-        }
+        setIsLoading(true)
         try {
-            response = await axios.post("https://cere.dev/users", userData)
-            if (response.status == 201) {
-                Alert.alert("Account creato con successo!")
-                navigation.goBack()
-            }
+            await checkConnection()
+            await axios.post(`${apiUrl}/api/users/signup`, user)
+            navigation.navigate("EmailConfirmation")
         } catch (error) {
-            console.log(error.response)
-            if (error.response.status == 409) {
-                Alert.alert("Un utente con queste credenziali esiste già")
-            }
-            // errore generato dal back-end quando non funza
-            else if (error.response.status == 502) {
-                Alert.alert("Qualcosa è andato storto")
-                // Qui non so quale sia l'errore, quindi lo printa e basta
-            } else {
-                Alert.alert(error.response.data.message)
-            }
+            console.log(error)
+            Alert.alert(error.response?.data || "Qualcosa è andato storto.")
+        } finally {
+            setIsLoading(false)
         }
-        return false
     };
 
     const initialValues = {
         nome: "",
         cognome: "",
-        classe: "",
         email: "",
         password: "",
         confirmPassword: ""
@@ -52,9 +41,7 @@ const useSignUp = (navigation) => {
         initialValues: initialValues,
         validationSchema: signUpSchema,
         onSubmit: async (values) => {
-            setIsLoading(true)
             await createUser(values, navigation)
-            setIsLoading(false)
         }
     })
 
