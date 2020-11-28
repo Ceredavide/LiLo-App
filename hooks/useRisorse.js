e0fc8e37f65c845e97987ab2ff99395d7b998a61
Davide <ceredavide@live.it>
2020-11-28 19:24:12 +0100

    creato hook useRisorse

---
+++ hooks/useRisorse.js
@@ -0,0 +1,58 @@
+import { useEffect, useState, useContext } from "react"
+
+import { AuthContext } from "../Context"
+
+import checkConnection from "../utils/checkConnection"
+
+import useEnvVars from "../configuration"
+const { apiUrl } = useEnvVars()
+
+export default function useRisorse() {
+
+    const { auth } = useContext(AuthContext)
+
+    const [isLoading, setIsLoading] = useState(false)
+    const [isRefreshing, setIsRefreshing] = useState(false)
+    const [risorse, setRisorse] = useState()
+    const [error, setError] = useState()
+
+
+    useEffect(() => { }, [])
+
+    async function fetchRisorse() {
+        try {
+            await checkConnection()
+            const response = await axios.get(`${apiUrl}/api/risorse`, {
+                headers: {
+                    Authorization: "Bearer " + auth.token
+                }
+            })
+
+            setRisorse(response.data.risorse)
+
+        } catch (error) {
+            setError(error)
+        }
+    }
+
+    const handleFetch = async () => {
+        setIsLoading(true)
+        await fetchRisorse()
+        setIsLoading(false)
+    }
+
+    const handleRefresh = async () => {
+        setIsRefreshing(true)
+        await fetchRisorse()
+        setIsRefreshing(false)
+    }
+
+    return {
+        risorse,
+        error,
+        isLoading,
+        isRefreshing,
+        handleRefresh,
+        handleFetch
+    }
+}
