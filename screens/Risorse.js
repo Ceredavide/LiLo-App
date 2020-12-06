cda811cc606daa88c2cf52da8d8e90252c8e4942
Davide <ceredavide@live.it>
2020-12-06 20:06:35 +0100

    Configurato Pagina

--- screens/Risorse.js
+++ screens/Risorse.js
@@ -1,21 +1,66 @@
 import React from "react"
-import { StyleSheet, View, Text } from "react-native"
-import Screen from "../components/shared/Screen"
+import { ActivityIndicator, FlatList, RefreshControl } from "react-native"
+import * as WebBrowser from 'expo-web-browser';
 
-const Risorse = () => {
+import useRisorse from "../hooks/useRisorse"
+
+import Error from "./Error"
+import Screen from "../components/shared/Screen"
+import TransitionView from "../components/shared/TransitionView"
+import IconButton from "../components/shared/IconButton"
+
+import Colors from "../constants/colors"
+
+const RisorseScreen = () => {
+
+    const {
+        risorse,
+        error,
+        isLoading,
+        isRefreshing,
+        handleRefresh,
+        handleFetch
+    } = useRisorse()
+
+    function renderItem({ item, index }) {
+        return (
+            <TransitionView index={index}>
+                <IconButton
+                    action={() => WebBrowser.openBrowserAsync(item.url)}
+                    iconName={item.iconName}
+                    text={item.nome}
+                    primaryColor={Colors.main}
+                    backgroundColor={Colors.white}
+                />
+            </TransitionView>
+        )
+    }
+
     return (
         <Screen>
-            <Text>Bella Mate</Text>
+            {
+                isLoading ?
+                    <ActivityIndicator color={Colors.white} />
+                    : error ?
+                        <Error text={error.response?.data || "Qualcosa è andato storto."} reload={handleFetch} />
+                        :
+                        <FlatList
+                            showsVerticalScrollIndicator={false}
+                            refreshing={isRefreshing}
+                            data={risorse}
+                            renderItem={renderItem}
+                            keyExtractor={item => item._id}
+                            refreshControl={
+                                <RefreshControl
+                                    refreshing={isRefreshing}
+                                    onRefresh={handleRefresh}
+                                    tintColor="#fff"
+                                />
+                            }
+                        />
+            }
         </Screen>
     )
 }
 
-// const Card = () => {
-
-// }
-
-const styles = StyleSheet.create({
-
-})
-
-export default Risorse
+export default RisorseScreen
