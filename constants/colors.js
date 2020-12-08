c315ac2be5dd8874fcc0b768ddfc278e789b2402
Davide <ceredavide@live.it>
2020-12-08 20:32:13 +0100

    modificato e aggiunto colori

--- constants/colors.js
+++ constants/colors.js
@@ -1,7 +1,10 @@
 const Colors = {
-    main: "#002C85",
-    secondary: "#009fff",
-    white: "#FFFF",
+    main: "#0D113E",
+    secondary: "#CCE2F2",
+    third: "#E34E20",
+    fourth: "#93A4BC",
+    white: "#ECF1F5",
+    black: "#000",
     green: "#4BB543",
     red: "#ff0033"
 }
