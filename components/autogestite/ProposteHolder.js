bbfc5100dc07833d4ae78f301d28f05afae32aa2
Davide <ceredavide@live.it>
2021-01-04 20:40:19 +0100

    commentato il tutto

--- components/autogestite/ProposteHolder.js
+++ components/autogestite/ProposteHolder.js
@@ -1,48 +1,48 @@
-import React from "react"
-import { StyleSheet, View } from "react-native"
+// import React from "react"
+// import { StyleSheet, View } from "react-native"
 
-import {
-    widthPercentageToDP as wp,
-    heightPercentageToDP as hp
-} from "react-native-responsive-screen";
+// import {
+//     widthPercentageToDP as wp,
+//     heightPercentageToDP as hp
+// } from "react-native-responsive-screen";
 
-import { Placeholder, PlaceholderLine, ShineOverlay } from "rn-placeholder"
+// import { Placeholder, PlaceholderLine, ShineOverlay } from "rn-placeholder"
 
-const CardPropostaHolder = () => {
-    return (
-        <View style={styles.container}>
-            <Placeholder Animation={ShineOverlay}>
-                <PlaceholderLine style={{ marginTop: hp("1%") }} width={45} />
-                <PlaceholderLine style={{ marginTop: hp("1%") }} width={95} />
-                <PlaceholderLine width={95} />
-            </Placeholder>
-        </View>
-    )
-}
+// const CardPropostaHolder = () => {
+//     return (
+//         <View style={styles.container}>
+//             <Placeholder Animation={ShineOverlay}>
+//                 <PlaceholderLine style={{ marginTop: hp("1%") }} width={45} />
+//                 <PlaceholderLine style={{ marginTop: hp("1%") }} width={95} />
+//                 <PlaceholderLine width={95} />
+//             </Placeholder>
+//         </View>
+//     )
+// }
 
-const ProposteHolder = () => {
-    const array = new Array(5).fill({})
-    return (array.map((e, i) => { return <CardPropostaHolder key={i} /> }))
-}
+// const ProposteHolder = () => {
+//     const array = new Array(5).fill({})
+//     return (array.map((e, i) => { return <CardPropostaHolder key={i} /> }))
+// }
 
-const styles = StyleSheet.create({
-    container: {
-        marginTop: hp("2%"),
-        marginHorizontal: wp("3%"),
-        alignSelf: "center",
-        alignItems: "center",
-        padding: 10,
-        borderRadius: 10,
-        backgroundColor: "#FFF",
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 1
-        },
-        shadowOpacity: 0.18,
-        shadowRadius: 1.0,
-        elevation: 1
-    }
-})
+// const styles = StyleSheet.create({
+//     container: {
+//         marginTop: hp("2%"),
+//         marginHorizontal: wp("3%"),
+//         alignSelf: "center",
+//         alignItems: "center",
+//         padding: 10,
+//         borderRadius: 10,
+//         backgroundColor: "#FFF",
+//         shadowColor: "#000",
+//         shadowOffset: {
+//             width: 0,
+//             height: 1
+//         },
+//         shadowOpacity: 0.18,
+//         shadowRadius: 1.0,
+//         elevation: 1
+//     }
+// })
 
-export default ProposteHolder
+// export default ProposteHolder
