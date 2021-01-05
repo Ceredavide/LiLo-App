4408369b6dfa9d9d1da87b3d2003e3267a46b2d7
Davide <ceredavide@live.it>
2021-01-05 23:33:43 +0100

    aggiunto Screen e Bottone errore credenziali

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet } from "react-native";
+import { StyleSheet, Alert, Linking } from "react-native";
 
 import {
   widthPercentageToDP as wp,
@@ -9,7 +9,9 @@ import {
 import { TextInput } from "react-native-paper"
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 
+import Screen from "../components/shared/Screen"
 import LoadingButton from "../components/shared/LoadingButton"
+import Button from "../components/shared/MyButton"
 import ErrorText from "../components/shared/ErrorText"
 
 import useSignUp from "../hooks/useSignUp";
@@ -25,86 +27,118 @@ const SignUp = ({ navigation }) => {
   const formTheme = { colors: { text: Colors.white, primary: Colors.secondary, placeholder: Colors.white, background: Colors.main } }
 
   return (
-    <KeyboardAwareScrollView showsVerticalScrollIndicator={false} style={styles.form}>
-      <TextInput
-        label="Nome"
-        error={errors.nome && touched.nome}
-        mode="outlined"
-        theme={formTheme}
-        style={{ ...styles.textInput, marginTop: 15 }}
-        onChangeText={handleChange("nome")}
-        onBlur={handleBlur("nome")}
-        value={values.nome}
-        returnKeyType="next"
-      />
-      <ErrorText error={errors.nome} touched={touched.nome} />
-      <TextInput
-        label="Cognome"
-        error={errors.cognome && touched.cognome}
-        mode="outlined"
-        theme={formTheme}
-        style={styles.textInput}
-        onChangeText={handleChange("cognome")}
-        onBlur={handleBlur("cognome")}
-        value={values.cognome}
-        returnKeyType="next"
-      />
-      <ErrorText error={errors.cognome} touched={touched.cognome} />
-      <TextInput
-        label="Email"
-        keyboardType="email-address"
-        error={errors.email && touched.email}
-        mode="outlined"
-        theme={formTheme}
-        style={styles.textInput}
-        onChangeText={handleChange("email")}
-        onBlur={handleBlur("email")}
-        value={values.email}
-        returnKeyType="next"
-      />
-      <ErrorText error={errors.email} touched={touched.email} />
-      <TextInput
-        label="Password"
-        secureTextEntry={true}
-        error={errors.password && touched.password}
-        mode="outlined"
-        theme={formTheme}
-        style={styles.textInput}
-        onChangeText={handleChange("password")}
-        onBlur={handleBlur("password")}
-        value={values.password}
-        returnKeyType="next"
-      />
-      <ErrorText error={errors.password} touched={touched.password} />
-      <TextInput
-        label="Conferma la password"
-        secureTextEntry={true}
-        error={errors.confirmPassword && touched.confirmPassword}
-        mode="outlined"
-        theme={formTheme}
-        style={styles.textInput}
-        onChangeText={handleChange("confirmPassword")}
-        onBlur={handleBlur("confirmPassword")}
-        value={values.confirmPassword}
-        returnKeyType="next"
-      />
-      <ErrorText error={errors.confirmPassword} touched={touched.confirmPassword} />
-      <LoadingButton
-        handleSubmit={handleSubmit}
-        loading={isLoading}
-        color={Object.entries(errors).length === 0 ? Colors.green : Colors.red}
-        text="Invia"
-        style={{ marginBottom: 30 }}
-      />
-    </KeyboardAwareScrollView>
+    <Screen>
+      <KeyboardAwareScrollView showsVerticalScrollIndicator={false} style={styles.form}>
+        <TextInput
+          label="Nome:"
+          error={errors.nome && touched.nome}
+          mode="outlined"
+          theme={formTheme}
+          style={{ ...styles.textInput, marginTop: 15 }}
+          onChangeText={handleChange("nome")}
+          onBlur={handleBlur("nome")}
+          value={values.nome}
+          returnKeyType="next"
+        />
+        <ErrorText error={errors.nome} touched={touched.nome} />
+        <TextInput
+          label="Cognome:"
+          error={errors.cognome && touched.cognome}
+          mode="outlined"
+          theme={formTheme}
+          style={styles.textInput}
+          onChangeText={handleChange("cognome")}
+          onBlur={handleBlur("cognome")}
+          value={values.cognome}
+          returnKeyType="next"
+        />
+        <ErrorText error={errors.cognome} touched={touched.cognome} />
+        <TextInput
+          label="Email:"
+          keyboardType="email-address"
+          error={errors.email && touched.email}
+          mode="outlined"
+          theme={formTheme}
+          style={styles.textInput}
+          onChangeText={handleChange("email")}
+          onBlur={handleBlur("email")}
+          value={values.email}
+          returnKeyType="next"
+        />
+        <ErrorText error={errors.email} touched={touched.email} />
+        <TextInput
+          label="Password:"
+          secureTextEntry={true}
+          error={errors.password && touched.password}
+          mode="outlined"
+          theme={formTheme}
+          style={styles.textInput}
+          onChangeText={handleChange("password")}
+          onBlur={handleBlur("password")}
+          value={values.password}
+          returnKeyType="next"
+        />
+        <ErrorText error={errors.password} touched={touched.password} />
+        <TextInput
+          label="Conferma la password:"
+          secureTextEntry={true}
+          error={errors.confirmPassword && touched.confirmPassword}
+          mode="outlined"
+          theme={formTheme}
+          style={styles.textInput}
+          onChangeText={handleChange("confirmPassword")}
+          onBlur={handleBlur("confirmPassword")}
+          value={values.confirmPassword}
+          returnKeyType="next"
+        />
+        <ErrorText error={errors.confirmPassword} touched={touched.confirmPassword} />
+        <LoadingButton
+          handleSubmit={handleSubmit}
+          loading={isLoading}
+          color={Object.entries(errors).length === 0 ? Colors.green : Colors.red}
+          text="Invia"
+          style={{ marginBottom: hp("1%") }}
+        />
+        <Button
+          text="Problemi a registrarsi?"
+          color={null}
+          action={toggleAlert}
+          style={{ width: wp("50%"), height: hp("3.5%") }}
+          textStyle={{ fontSize: hp("1.7%"), color: Colors.warning }}
+        />
+      </KeyboardAwareScrollView>
+    </Screen>
   );
 };
+
+function toggleAlert() {
+  Alert.alert(
+    "Mo' che si fa?",
+    'Può capitare che le tue credenziali di accesso risultino errate anche se non è il caso, se fosse così invia una mail a davide@cere.dev da una email attendibile con scritto nome, cognome e classe',
+    [
+      {
+        text: 'Annulla',
+        onPress: () => { },
+        style: 'cancel'
+      },
+      {
+        text: 'Invia Mail',
+        onPress: () => openMailer()
+      }
+    ],
+    { cancelable: false }
+  );
+}
+
+function openMailer() {
+  Linking.openURL('mailto:support@example.com')
+}
 
 const styles = StyleSheet.create({
   form: {
     flex: 1,
     backgroundColor: Colors.main,
-    paddingVertical: hp("5%")
+    marginTop: hp("4%")
   },
   textInput: {
     width: wp("65%"),
