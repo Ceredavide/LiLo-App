91d8260cea32627827999a1299a7f5ecd5aef675
Davide <ceredavide@live.it>
2021-01-18 11:33:40 +0100

    aggiornato func getEnvVars

--- configuration.js
+++ configuration.js
@@ -1,25 +1,32 @@
 import Constants from "expo-constants";
 
 const ENV = {
-    dev: {
-        apiUrl: "https://cere.dev/lilo"
-    },
-    lilo: {
-        apiUrl: "https://cere.dev/lilo"
-    },
-    lilu1: {
-        apiUrl: ""
-    }
+  dev: {
+    apiUrl: "https://cere.dev/lilo"
+  },
+  lilo: {
+    apiUrl: "https://cere.dev/lilo"
+  },
+  lilu: {
+    apiUrl: ""
+  }
 };
 
-const getEnvVars = (env = Constants.manifest.releaseChannel) => {
-    if (__DEV__) {
+const getEnvVars = () => {
+
+  const env = Constants.manifest.releaseChannel;
+
+  if (!__DEV__ && env) {
+    switch (env) {
+      case env.indexOf("lilo") !== -1:
+        return ENV.lilo;
+      case env.indexOf("lilu") !== -1:
+        return ENV.lilu;
+      default:
         return ENV.dev;
-    } else if (env === 'LiLo') {
-        return ENV.LiLo;
-    } else if (env === 'LiLu1') {
-        return ENV.LiLu1;
     }
+  }
+  return ENV.dev;
 };
 
 export default getEnvVars;
