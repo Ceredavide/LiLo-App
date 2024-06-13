eab7537ee96ce666cdb9d9d66cdafe6e30d96687
Davide Ceresa <ceredavide@live.it>
2024-06-13 11:23:58 +0200

    refactor: removed deprecated expo-app-loading

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from "react"
+import React, { useState, useEffect ,useMemo } from "react"
 import { Provider } from "react-redux"
 
 import { NavigationContainer } from "@react-navigation/native"
@@ -11,19 +11,65 @@ import store from "./src/store/store"
 
 import { AuthContext } from "./src/Context"
 
-import Loading from "./src/screens/Loading";
+import { useFonts } from 'expo-font';
+import {Asset} from "expo-asset";
+import * as SecureStore from "expo-secure-store";
 
 const { Navigator, Screen } = createStackNavigator()
 
 const App = () => {
 
-    const [isLoading, setIsLoading] = useState(true)
+    const [appIsReady, setAppIsReady] = useState(true)
     const [auth, setAuth] = useState({})
+
+    const [fontsLoaded] = useFonts({
+        "open-sans-light": require("./assets/fonts/OpenSans-Light.ttf"),
+        "open-sans-regular": require("./assets/fonts/OpenSans-Regular.ttf"),
+        "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+        "PlayFair-SemiBold": require("./assets/fonts/PlayfairDisplay-SemiBold.ttf")
+    })
+
+    useEffect(() => {
+        async function prepare() {
+            try {
+
+                const cachedData = await SecureStore.getItemAsync("user")
+
+                if (cachedData) {
+                    const parsedData = await JSON.parse(cachedData)
+                    setAuth(parsedData)
+                }
+
+                return Promise.all(
+                    // Load assets
+                    await Asset.loadAsync([
+                        require("./assets/images/icon.png"),
+                        require("./assets/images/logo.png"),
+                        require("./assets/images/scuola.jpeg"),
+                        require("./assets/images/student-hat.png"),
+                        require("./assets/images/wallpaper.png"),
+                        require("./assets/images/illustrations/autogestite.jpg"),
+                        require("./assets/images/illustrations/login.png"),
+                        require("./assets/images/illustrations/mappa.jpg"),
+                        require("./assets/images/illustrations/qr.jpg"),
+                        require("./assets/images/illustrations/risorse.jpg"),
+                    ]),
+                )
+            } catch (e) {
+                console.warn(e);
+            } finally {
+                // Tell the application to render
+                setAppIsReady(true);
+            }
+        }
+
+        prepare();
+    }, []);
 
     const providerAuth = useMemo(() => ({ auth, setAuth }), [auth, setAuth])
 
-    if (isLoading) {
-        return <Loading setIsLoading={setIsLoading} setAuth={setAuth} />
+    if (!appIsReady || !fontsLoaded) {
+        return null
     }
 
     else {
@@ -33,12 +79,8 @@ const App = () => {
                     <NavigationContainer>
                         <Navigator screenOptions={_screenOptions} mode="modal">
                             {!auth.token ?
-                                <>
-                                    <Screen name="Auth" component={AuthStack} />
-                                </> :
-                                <>
-                                    <Screen name="App" component={AppNavigator} />
-                                </>
+                                <Screen name="Auth" component={AuthStack}/> :
+                                <Screen name="App" component={AppNavigator}/>
                             }
                         </Navigator>
                     </NavigationContainer>
